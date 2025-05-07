14296eb83fffa4069f7ad6f08ab203cf1918e129
Jayesh Bhole <jayeshbhole123@gmail.com>
2025-05-07 21:43:35 +0530

    chore: local account setup

--- src/components/provider-setup/code.tsx
+++ src/components/provider-setup/code.tsx
@@ -1,27 +1,27 @@
 import { CodeBlockProps } from "../ui/code";
 
-export const privySetupCode: Array<CodeBlockProps & { stepTitle?: string; stepDescription?: string }> = [
+export const localSetupCode: Array<CodeBlockProps & { stepTitle?: string; stepDescription?: string }> = [
   {
     stepTitle: "Installing Dependencies",
     type: "command",
     packageManagers: [
       {
         type: "npm",
-        command: "npm i @privy-io/react-auth @tanstack/react-query wagmi viem",
+        command: "npm i @tanstack/react-query wagmi viem",
       },
       {
         type: "yarn",
-        command: "yarn add @privy-io/react-auth @tanstack/react-query wagmi viem",
+        command: "yarn add @tanstack/react-query wagmi viem",
       },
       {
         type: "pnpm",
-        command: "pnpm add @privy-io/react-auth @tanstack/react-query wagmi viem",
+        command: "pnpm add @tanstack/react-query wagmi viem",
       },
     ],
   },
   {
     type: "files",
-    stepDescription: "Setup the Privy context with your credentials. Initialise the 7702 client as follows.",
+    stepDescription: "Initialise the 7702 client with Viem and Wagmi as follows.",
     files: [
       {
         name: "client.ts",
@@ -29,92 +29,53 @@ export const privySetupCode: Array<CodeBlockProps & { stepTitle?: string; stepDe
         content: `const kernelVersion = KERNEL_V3_3;
 const kernelAddresses = KernelVersionToAddressesMap[kernelVersion];
 
-// get wallet client from privy
-const privyEmbeddedWallet = useMemo(() => {
-    return wallets.find((wallet) => wallet.walletClientType === "privy");
-}, [wallets]);
+const newPrivateKey = generatePrivateKey();
+const newAccount = privateKeyToAccount(newPrivateKey);
 
-const walletClient = useQuery({
-    queryKey: ['privy', "walletClient", privyEmbeddedWallet?.address],
-    queryFn: async () => {
-      if (!privyEmbeddedWallet) {
-        return null;
-      }
-    return createWalletClient({
-      account: privyEmbeddedWallet.address as Hex,
-      chain: baseSepolia,
-      });
-    },
-    enabled: !!privyEmbeddedWallet,
+const baseSepoliaPublicClient = usePublicClient({
+  chainId: baseSepolia.id,
 });
 
-const { data: kernelClients } = useQuery({
-    queryKey: [
-        'privy',
-        "kernelClients",
-        walletClient?.account.address,
-        baseSepoliaPaymasterClient?.name,
-        baseSepoliaPublicClient?.name,
-    ],
-    queryFn: async () => {
-        if (!walletClient || !baseSepoliaPublicClient || !baseSepoliaPaymasterClient) return null;
+const authorization = await account.signAuthorization({
+  chainId: baseSepolia.id,
+  nonce: 0,
+  address: kernelAddresses.accountImplementationAddress,
+});
 
-        const ecdsaValidator = await signerToEcdsaValidator(baseSepoliaPublicClient, {
-            signer: walletClient,
-            entryPoint: getEntryPoint("0.7"),
-            kernelVersion: KERNEL_V3_3,
-        });
+const kernelAccount = await create7702KernelAccount(baseSepoliaPublicClient, {
+  signer: account,
+  entryPoint,
+  kernelVersion,
+  eip7702Auth: authorization,
+});
 
-        const authorization = await signAuthorization({
-            contractAddress: kernelAddresses.accountImplementationAddress, // The address of the smart contract
-            chainId: baseSepolia.id,
-        });
+const kernelAccountClient = create7702KernelAccountClient({
+  account: kernelAccount,
+  chain: baseSepolia,
+  bundlerTransport: http(baseSepoliaBundlerRpc),
+  paymaster: baseSepoliaPaymasterClient,
+  client: baseSepoliaPublicClient,
+});
 
-        const kernelAccount = await createKernelAccount(baseSepoliaPublicClient, {
-            plugins: {
-                sudo: ecdsaValidator,
-            },
-            entryPoint: getEntryPoint("0.7"),
-            kernelVersion: KERNEL_V3_3,
-            address: walletClient.account.address,
-            eip7702Auth: authorization,
-        });
-
-        const kernelAccountClient = createKernelAccountClient({
-            account: kernelAccount,
-            chain: baseSepolia,
-            bundlerTransport: http(baseSepoliaBundlerRpc),
-            paymaster: baseSepoliaPaymasterClient,
-            client: baseSepoliaPublicClient,
-        });
-
-        return { kernelAccountClient, kernelAccount, ecdsaValidator };
-    },
-    enabled: !!baseSepoliaPublicClient && !!walletClient && !!baseSepoliaPaymasterClient,
+const ecdsaValidator = await signerToEcdsaValidator(baseSepoliaPublicClient, {
+  signer: account,
+  entryPoint,
+  kernelVersion,
 });
 `,
       },
       {
         name: "context.ts",
         language: "react",
-        content: `import { PrivyProvider } from "@privy-io/react-auth";
-
-// const wagmiConfig = ...;
+        content: `const wagmiConfig = createConfig({
+  chains: [baseSepolia],
+  transports: {
+    [baseSepolia.id]: http(),
+  },
+});
 
 <WagmiProvider config={wagmiConfig}>
-    <PrivyProvider
-        appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
-        clientId={process.env.NEXT_PUBLIC_CLIENT_ID}
-        config={{
-            // Create embedded wallets for users who don't have a wallet
-            embeddedWallets: {
-                showWalletUIs: true,
-                createOnLogin: "all-users",
-            },
-        }}
-    >
-        {children}
-    </PrivyProvider>
+  <LocalAccountProvider>{children}</LocalAccountProvider>
 </WagmiProvider>`,
       },
     ],
