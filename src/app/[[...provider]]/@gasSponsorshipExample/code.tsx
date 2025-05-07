6d54a0f70cb4a5edea6935c701df9f99eef69f41
Jayesh Bhole <jayeshbhole123@gmail.com>
2025-05-07 22:54:04 +0530

    chore: fix gas sponsorship files

--- src/app/[[...provider]]/@gasSponsorshipExample/code.tsx
+++ src/app/[[...provider]]/@gasSponsorshipExample/code.tsx
@@ -5,20 +5,6 @@ const gasSponsorshipExampleCode: Array<CodeBlockProps & { stepTitle?: string }> 
     type: "files",
     files: [
       {
-        name: "sendTransaction.ts",
-        language: "typescript",
-        content: `kernelAccountClient.sendTransaction({
-  account: kernelAccount,
-  to: ZERODEV_TOKEN_ADDRESS,
-  value: BigInt(0),
-  data: encodeFunctionData({
-    abi: ZERODEV_TOKEN_ABI,
-    functionName: "mint",
-    args: [kernelAccount.address, amount],
-  }),
-})`,
-      },
-      {
         name: "kernelClient.ts",
         language: "typescript",
         content: `const paymasterClient = createZeroDevPaymasterClient({
@@ -37,6 +23,20 @@ const kernelAccountClient = createKernelAccountClient({
   client: baseSepoliaPublicClient,
 });
 `,
+      },
+      {
+        name: "sendTransaction.ts",
+        language: "typescript",
+        content: `kernelAccountClient.sendTransaction({
+  account: kernelAccount,
+  to: ZERODEV_TOKEN_ADDRESS,
+  value: BigInt(0),
+  data: encodeFunctionData({
+    abi: ZERODEV_TOKEN_ABI,
+    functionName: "mint",
+    args: [kernelAccount.address, amount],
+  }),
+})`,
       },
     ],
   },
